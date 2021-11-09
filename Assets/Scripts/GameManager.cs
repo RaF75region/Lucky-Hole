e3549559dde82b66b5f73b55b2b659ff8ab19684
al.kolobov <aleks.92@inbox.ru>
2021-11-09 16:01:54 +0700

    Добавление respawn

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Client")]
+    public GameObject prefabClient;
+    public GameObject PrefabPointClient;
+    public float timerRespawn;
+    [SerializeField]
+    bool createClient = false;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerRespawn = Random.Range(3f, 7f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RespawnClient();
+    }
+
+    private void RespawnClient()
+    {
+        if (!createClient)
+        {
+            timerRespawn -= Time.deltaTime;
+            if (timerRespawn < 0)
+            {
+                Instantiate(prefabClient);
+                timerRespawn = Random.Range(3f, 7f);
+                createClient = true;
+            }
+        }
     }
 }
