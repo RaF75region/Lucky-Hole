c3f395b5f81126a0cf965770f43afc8f281d6654
al.kolobov <aleks.92@inbox.ru>
2021-11-13 13:17:11 +0700

    Изменение работы камеры

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
     public Transform Floor;
-    public float SpeedMOve = 1f;
+    public float SpeedMove = 10f;
+    public float boundsX = 10f;
+    public int boundsZ = 10;
     // Update is called once per frame
     void Update()
     {
-        float xMove = Input.GetAxis("Horizontal") * Time.deltaTime * SpeedMOve;
-        float zMove = Input.GetAxis("Vertical") * Time.deltaTime * SpeedMOve;
+        float xMove = Input.GetAxis("Horizontal") * Time.deltaTime * SpeedMove;
+        float zMove = Input.GetAxis("Vertical") * Time.deltaTime * SpeedMove;
         Vector3 position = transform.position;
         position.x += xMove;
         position.z += zMove;
+        position.x = Mathf.Clamp(position.x, -boundsX, boundsX);
+        position.z = Mathf.Clamp(position.z, -boundsZ, boundsZ);
         transform.position = position;
 
         
